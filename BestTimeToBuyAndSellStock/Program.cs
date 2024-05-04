10d4635875d5e3669a2756bcdecd8e8256a3c540
ivalchanka <i.valchenk@hotmail.com>
2024-05-04 19:51:55 +0400

    Pushed one more task.

---
+++ BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace BestTimeToBuyAndSellStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testItems = new[]
+            {
+                new TestItem
+                {
+                    InputArray = new [] { 7, 1, 5, 3, 6, 4 },
+                    Result = 7
+                },
+                new TestItem
+                {
+                    InputArray = new [] { 1, 2, 3, 4, 5 },
+                    Result = 4
+                },
+                new TestItem
+                {
+                    InputArray = new [] { 7, 6, 4, 3, 1 },
+                    Result = 0
+                }
+            };
+
+            foreach (var testItem in testItems)
+            {
+                var actualResult = MaxProfit(testItem.InputArray);
+
+                if (actualResult != testItem.Result)
+                {
+                    Console.WriteLine($"\n\nFAILED. Expected: {testItem.Result}. Actual: {actualResult}. Input array: [{ToString(testItem.InputArray)}].");
+                }
+            }
+
+            Console.WriteLine("Tap to continue...");
+            Console.ReadKey();
+        }
+
+        private static int MaxProfit(int[] prices)
+        {
+            if (prices.Length == 1)
+            {
+                return 0;
+            }
+
+            var profit = 0;
+            var buyPrice = prices[0];
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (buyPrice >= prices[i])
+                {
+                    // Change buy price.
+                    buyPrice = prices[i];
+                }
+
+                if (i + 1 != prices.Length && buyPrice > prices[i+1])
+                {
+                    profit = prices[i + 1] - buyPrice;
+                }
+            }
+
+            return profit;
+        }
+
+        private static string ToString<T>(T[] array)
+        {
+            return string.Join(",", array);
+        }
+    }
+}
---
+++ BestTimeToBuyAndSellStock/TestItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BestTimeToBuyAndSellStock
+{
+    public class TestItem
+    {
+        public int[] InputArray { get; set; }
+        public int Result { get; set; }
+    }
+}
